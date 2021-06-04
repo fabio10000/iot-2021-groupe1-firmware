30d8c8d2bb319994e438110cf09392a9bebb7866
Fabio <fabiomanuelsilvamarques@gmail.com>
2021-06-04 17:38:01 +0200

    updated env click script

--- environment_click.js
+++ environment_click.js
@@ -1,30 +1,37 @@
 const DELAY = 3 * 60000;
 // minified url to local modules on github repo
-var RN2483 = require("https://git.io/JsgXY");
+const RN2483 = require("https://git.io/JsgXY");
+const Parser = require("parser"); //todo require from github
+
 Serial3.setup(57600/25*8, { tx:D8, rx:D9 });
 var lora = new RN2483(Serial3, {reset: E13, debug: true});
 var i2c = new I2C();
 i2c.setup({sda:C9,scl:A8});
 var bme = require("BME680").connectI2C(i2c, {addr:0x77});
 
-function decimal_to_hex(d, padding) {
-  var hex = Number(d).toString(16);
-  padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
-
-  while (hex.length < padding) {
-      hex = "0" + hex;
-  }
-
-  return hex;
-}
-
 function convert_to_payload(json) {
-  var payload = decimal_to_hex(Math.round(json.temperature));
-  payload += decimal_to_hex(Math.round(json.humidity));
-  payload += decimal_to_hex(Math.round(json.pressure), 4);
+  // todo: vérifier les arrondis
+  var payload = Parser.encode_payload({
+    "3303": json.temperature,
+    "3304": json.humidity,
+    "3315": json.pressure
+  })
 
   return payload;
 }
+
+lora.on('message', function(d) {
+  var data = Parser.decode_payload(d)
+
+  //todo vérifier la valeur choisie
+  if (data["3302"] > 12) {
+    digitalWrite(E12, 1);
+    digitalWrite(E15, 0);
+  } else {
+    digitalWrite(E15, 1);
+    digitalWrite(E12, 0);
+  }
+});
 
 lora.on('connexion', function(d) {
   if (d == 'denied') {
